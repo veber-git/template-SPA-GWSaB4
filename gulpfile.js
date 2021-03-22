7f41758a8bc0d78e38e88d1a977330702c38e2e1
veber-git <veber-git@mail.ru>
2021-03-22 19:16:45 +0300

    added merging of js-files of libraries to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,76 @@
 let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
-		fileswatch   = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+	fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
 
-const { src, dest, parallel, series, watch } = require('gulp')
-const browserSync  = require('browser-sync').create()
-const bssi         = require('browsersync-ssi')
-const ssi          = require('ssi')
-const webpack      = require('webpack-stream')
-const sass         = require('gulp-sass')
-const sassglob     = require('gulp-sass-glob')
-const less         = require('gulp-less')
-const lessglob     = require('gulp-less-glob')
-const styl         = require('gulp-stylus')
-const stylglob     = require("gulp-noop")
-const cleancss     = require('gulp-clean-css')
+const {
+	src,
+	dest,
+	parallel,
+	series,
+	watch
+} = require('gulp')
+const browserSync = require('browser-sync').create()
+const bssi = require('browsersync-ssi')
+const ssi = require('ssi')
+const webpack = require('webpack-stream')
+const sass = require('gulp-sass')
+const sassglob = require('gulp-sass-glob')
+const less = require('gulp-less')
+const lessglob = require('gulp-less-glob')
+const styl = require('gulp-stylus')
+const stylglob = require("gulp-noop")
+const cleancss = require('gulp-clean-css')
 const autoprefixer = require('gulp-autoprefixer')
-const rename       = require('gulp-rename')
-const imagemin     = require('gulp-imagemin')
-const newer        = require('gulp-newer')
-const rsync        = require('gulp-rsync')
-const del          = require('del')
+const rename = require('gulp-rename')
+const imagemin = require('gulp-imagemin')
+const newer = require('gulp-newer')
+const rsync = require('gulp-rsync')
+const del = require('del')
+const concat = require('gulp-concat')
 
 function browsersync() {
 	browserSync.init({
 		server: {
 			baseDir: 'app/',
-			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+			middleware: bssi({
+				baseDir: 'app/',
+				ext: '.html'
+			})
 		},
-		ghostMode: { clicks: false },
+		ghostMode: {
+			clicks: false
+		},
 		notify: false,
 		online: true,
 		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
 	})
 }
 
+function mergerJs() {
+	return src([
+			'node_modules/jquery/dist/jquery.min.js'
+		])
+		.pipe(concat('libs.min.js'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
 function scripts() {
 	return src(['app/js/*.js', '!app/js/*.min.js'])
 		.pipe(webpack({
 			mode: 'production',
-			performance: { hints: false },
+			performance: {
+				hints: false
+			},
 			module: {
-				rules: [
-					{
-						test: /\.(js)$/,
-						exclude: /(node_modules)/,
-						loader: 'babel-loader',
-						query: {
-							presets: ['@babel/env'],
-							plugins: ['babel-plugin-root-import']
-						}
+				rules: [{
+					test: /\.(js)$/,
+					exclude: /(node_modules)/,
+					loader: 'babel-loader',
+					query: {
+						presets: ['@babel/env'],
+						plugins: ['babel-plugin-root-import']
 					}
-				]
+				}]
 			}
 		})).on('error', function handleError() {
 			this.emit('end')
@@ -63,9 +84,21 @@ function styles() {
 	return src([`app/${preprocessor}/*.*`, `!app/${preprocessor}/_*.*`])
 		.pipe(eval(`${preprocessor}glob`)())
 		.pipe(eval(preprocessor)())
-		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-		.pipe(cleancss({ level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
-		.pipe(rename({ suffix: ".min" }))
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 10 versions'],
+			grid: true
+		}))
+		.pipe(cleancss({
+			level: {
+				1: {
+					specialComments: 0
+				}
+			},
+			/* format: 'beautify' */
+		}))
+		.pipe(rename({
+			suffix: ".min"
+		}))
 		.pipe(dest('app/css'))
 		.pipe(browserSync.stream())
 }
@@ -80,22 +113,28 @@ function images() {
 
 function buildcopy() {
 	return src([
-		'{app/js,app/css}/*.min.*',
-		'app/images/**/*.*',
-		'!app/images/src/**/*',
-		'app/fonts/**/*'
-	], { base: 'app/' })
-	.pipe(dest('dist'))
+			'{app/js,app/css}/*.min.*',
+			'app/images/**/*.*',
+			'!app/images/src/**/*',
+			'app/fonts/**/*'
+		], {
+			base: 'app/'
+		})
+		.pipe(dest('dist'))
 }
 
 async function buildhtml() {
 	let includes = new ssi('app/', 'dist/', '/**/*.html')
 	includes.compile()
-	del('dist/parts', { force: true })
+	del('dist/parts', {
+		force: true
+	})
 }
 
 function cleandist() {
-	return del('dist/**/*', { force: true })
+	return del('dist/**/*', {
+		force: true
+	})
 }
 
 function deploy() {
@@ -105,8 +144,8 @@ function deploy() {
 			hostname: 'username@yousite.com',
 			destination: 'yousite/public_html/',
 			// clean: true, // Mirror copy with file deletion
-			include: [/* '*.htaccess' */], // Included files to deploy,
-			exclude: [ '**/Thumbs.db', '**/*.DS_Store' ],
+			include: [ /* '*.htaccess' */ ], // Included files to deploy,
+			exclude: ['**/Thumbs.db', '**/*.DS_Store'],
 			recursive: true,
 			archive: true,
 			silent: false,
@@ -115,16 +154,25 @@ function deploy() {
 }
 
 function startwatch() {
-	watch(`app/${preprocessor}/**/*`, { usePolling: true }, styles)
-	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
-	watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', { usePolling: true }, images)
-	watch(`app/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload)
+	watch(`app/${preprocessor}/**/*`, {
+		usePolling: true
+	}, styles)
+	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], {
+		usePolling: true
+	}, scripts)
+	watch('app/images/src/**/*.{jpg,jpeg,png,webp,svg,gif}', {
+		usePolling: true
+	}, images)
+	watch(`app/**/*.{${fileswatch}}`, {
+		usePolling: true
+	}).on('change', browserSync.reload)
 }
 
+exports.mergerJs = mergerJs
 exports.scripts = scripts
-exports.styles  = styles
-exports.images  = images
-exports.deploy  = deploy
-exports.assets  = series(scripts, styles, images)
-exports.build   = series(cleandist, scripts, styles, images, buildcopy, buildhtml)
-exports.default = series(scripts, styles, images, parallel(browsersync, startwatch))
+exports.styles = styles
+exports.images = images
+exports.deploy = deploy
+exports.assets = series(mergerJs, scripts, styles, images)
+exports.build = series(cleandist, mergerJs, scripts, styles, images, buildcopy, buildhtml)
+exports.default = series(mergerJs, scripts, styles, images, parallel(browsersync, startwatch))
